380b4b58c91f282fc3d18f7f5152a2f9a1669951
sarahatem5 <167104527+sarahatem5@users.noreply.github.com>
2024-04-15 17:46:58 +0200

    Add files via upload

---
+++ comp/comparison.js
@@ -0,0 +1,235 @@
+/*function sara(x){
+    switch(x){
+        case 1:
+            document.getElementById("a").innerHTML = "769 hp";
+            break;
+        case 2:
+            document.getElementById("a").innerHTML = "819 hp";
+            break;
+        case 3:
+            document.getElementById("a").innerHTML = "612 hp";
+            break;
+        case 4:
+            document.getElementById("a").innerHTML = "950 hp";
+            break;
+        case 5:
+            document.getElementById("a").innerHTML = "690 hp";
+            break;
+        case 6:
+            document.getElementById("a").innerHTML = "640 hp";
+            break;
+        case 7:
+            document.getElementById("a").innerHTML = "443 hp";
+            break;
+        case 8:
+            document.getElementById("a").innerHTML = "350 hp";
+            break;
+        case 9:
+            document.getElementById("a").innerHTML = "671 hp";
+            break;
+        case 10:
+            document.getElementById("a").innerHTML = "715 hp";
+            break;
+        case 11:
+            document.getElementById("a").innerHTML = "528 hp";
+            break;
+        case 12:
+            document.getElementById("a").innerHTML = "1139 hp";
+            break;
+        case 13:
+            document.getElementById("a").innerHTML = "602 hp";
+            break;
+        case 14:
+            document.getElementById("a").innerHTML = "770 hp";
+            break;
+        case 15:
+            document.getElementById("a").innerHTML = "5000 hp";
+            break;
+    }
+}*/
+
+function sara(x){
+    if(x==1){
+        document.getElementById("a").innerHTML = "769 hp";
+        document.getElementById("b").innerHTML = "v8";
+        document.getElementById("c").innerHTML = "2.5sec";
+        document.getElementById("d").innerHTML = "850,000$";
+    }
+    else if(x==2){
+        document.getElementById("a").innerHTML = "819 hp";
+        document.getElementById("b").innerHTML = "v6";
+        document.getElementById("c").innerHTML = "2.7sec";
+        document.getElementById("d").innerHTML = "390,000$";
+    }
+    else if(x==3){
+        document.getElementById("a").innerHTML = "612 hp";
+        document.getElementById("b").innerHTML = "v8 ,4000 cc";
+        document.getElementById("c").innerHTML = "3.4sec";
+        document.getElementById("d").innerHTML = "247,300$";
+    }
+    else if(x==4){
+        document.getElementById("a").innerHTML = "950 hp";
+        document.getElementById("b").innerHTML = "v12 ,6300cc";
+        document.getElementById("c").innerHTML = "2.7sec";
+        document.getElementById("d").innerHTML = "1M$";
+    }
+    else if(x==5){
+        document.getElementById("a").innerHTML = "690 hp";
+        document.getElementById("b").innerHTML = "v6 ,3800cc";
+        document.getElementById("c").innerHTML = "3.5sec";
+        document.getElementById("d").innerHTML = "344,800$";
+    }
+    else if(x==6){
+        document.getElementById("a").innerHTML = "640 hp";
+        document.getElementById("b").innerHTML = "v6 ,3800cc";
+        document.getElementById("c").innerHTML = "2.7sec";
+        document.getElementById("d").innerHTML = "275,800$";
+    }
+    else if(x==7){
+        document.getElementById("a").innerHTML = "443 hp";
+        document.getElementById("b").innerHTML = "v6,3000cc";
+        document.getElementById("c").innerHTML = "2.7sec";
+        document.getElementById("d").innerHTML = "114,000$";
+    }
+    else if(x==8){
+        document.getElementById("a").innerHTML = "350 hp";
+        document.getElementById("b").innerHTML = "4 cylinder ,2000cc";
+        document.getElementById("c").innerHTML = "4.9sec";
+        document.getElementById("d").innerHTML = "105,000$";
+    }
+    else if(x==9){
+        document.getElementById("a").innerHTML = "671 hp";
+        document.getElementById("b").innerHTML = "v8 ,twin turbo";
+        document.getElementById("c").innerHTML = "3.6sec";
+        document.getElementById("d").innerHTML = "270,000$";
+    }
+    else if(x==10){
+        document.getElementById("a").innerHTML = "715 hp";
+        document.getElementById("b").innerHTML = "v12 ,bi-turbo";
+        document.getElementById("c").innerHTML = "3.4sec";
+        document.getElementById("d").innerHTML = "423,000$";
+    }
+    else if(x==11){
+        document.getElementById("a").innerHTML = "528 hp";
+        document.getElementById("b").innerHTML = "v8 ,twin turbo";
+        document.getElementById("c").innerHTML = "3.5sec";
+        document.getElementById("d").innerHTML = "210,000$";
+    }
+    else if(x==12){
+        document.getElementById("a").innerHTML = "1139 hp";
+        document.getElementById("b").innerHTML = "6.5L/v12 ,electric motor";
+        document.getElementById("c").innerHTML = "2.6sec";
+        document.getElementById("d").innerHTML = "450,000$";
+    }
+    else if(x==13){
+        document.getElementById("a").innerHTML = "602 hp";
+        document.getElementById("b").innerHTML = "v10 NA";
+        document.getElementById("c").innerHTML = "2.4sec";
+        document.getElementById("d").innerHTML = "250,000$";
+    }
+    else if(x==14){
+        document.getElementById("a").innerHTML = "770 hp";
+        document.getElementById("b").innerHTML = "v12,6500cc";
+        document.getElementById("c").innerHTML = "2.9sec";
+        document.getElementById("d").innerHTML = "507,000$";
+    }
+    else if(x==15){
+        document.getElementById("a").innerHTML = "5000 hp";
+        document.getElementById("b").innerHTML = "v10,5000cc";
+        document.getElementById("c").innerHTML = "4.1sec";
+        document.getElementById("d").innerHTML = "177,890$";
+    }
+    }
+    function soso(x){
+        if(x==111){
+            document.getElementById("aa").innerHTML = "769 hp";
+            document.getElementById("bb").innerHTML = "v8";
+            document.getElementById("cc").innerHTML = "2.5sec";
+            document.getElementById("dd").innerHTML = "850,000$";
+        }
+        else if(x==222){
+            document.getElementById("aa").innerHTML = "819 hp";
+            document.getElementById("bb").innerHTML = "v6";
+            document.getElementById("cc").innerHTML = "2.7sec";
+            document.getElementById("dd").innerHTML = "390,000$";
+        }
+        else if(x==333){
+            document.getElementById("aa").innerHTML = "612 hp";
+            document.getElementById("bb").innerHTML = "v8 ,4000 cc";
+            document.getElementById("cc").innerHTML = "3.4sec";
+            document.getElementById("dd").innerHTML = "247,300$";
+        }
+        else if(x==444){
+            document.getElementById("aa").innerHTML = "950 hp";
+            document.getElementById("bb").innerHTML = "v12 ,6300cc";
+            document.getElementById("cc").innerHTML = "2.7sec";
+            document.getElementById("dd").innerHTML = "1M$";
+        }
+        else if(x==555){
+            document.getElementById("aa").innerHTML = "690 hp";
+            document.getElementById("bb").innerHTML = "v6 ,3800cc";
+            document.getElementById("cc").innerHTML = "3.5sec";
+            document.getElementById("dd").innerHTML = "344,800$";
+        }
+        else if(x==666){
+            document.getElementById("aa").innerHTML = "640 hp";
+            document.getElementById("bb").innerHTML = "v6 ,3800cc";
+            document.getElementById("cc").innerHTML = "2.7sec";
+            document.getElementById("dd").innerHTML = "275,800$";
+        }
+        else if(x==777){
+            document.getElementById("aa").innerHTML = "443 hp";
+            document.getElementById("bb").innerHTML = "v6,3000cc";
+            document.getElementById("cc").innerHTML = "2.7sec";
+            document.getElementById("dd").innerHTML = "114,000$";
+        }
+        else if(x==888){
+            document.getElementById("aa").innerHTML = "350 hp";
+            document.getElementById("bb").innerHTML = "4 cylinder ,2000cc";
+            document.getElementById("cc").innerHTML = "4.9sec";
+            document.getElementById("dd").innerHTML = "105,000$";
+        }
+        else if(x==999){
+            document.getElementById("aa").innerHTML = "671 hp";
+            document.getElementById("bb").innerHTML = "v8 ,twin turbo";
+            document.getElementById("cc").innerHTML = "3.6sec";
+            document.getElementById("dd").innerHTML = "270,000$";
+        }
+        else if(x==110){
+            document.getElementById("aa").innerHTML = "715 hp";
+            document.getElementById("bb").innerHTML = "v12 ,bi-turbo";
+            document.getElementById("cc").innerHTML = "3.4sec";
+            document.getElementById("dd").innerHTML = "423,000$";
+        }
+        else if(x==1111){
+            document.getElementById("aa").innerHTML = "528 hp";
+            document.getElementById("bb").innerHTML = "v8 ,twin turbo";
+            document.getElementById("cc").innerHTML = "3.5sec";
+            document.getElementById("dd").innerHTML = "210,000$";
+        }
+        else if(x==112){
+            document.getElementById("aa").innerHTML = "1139 hp";
+            document.getElementById("bb").innerHTML = "6.5L/v12 ,electric motor";
+            document.getElementById("cc").innerHTML = "2.6sec";
+            document.getElementById("dd").innerHTML = "450,000$";
+        }
+        else if(x==113){
+            document.getElementById("aa").innerHTML = "602 hp";
+            document.getElementById("bb").innerHTML = "v10 NA";
+            document.getElementById("cc").innerHTML = "2.4sec";
+            document.getElementById("dd").innerHTML = "250,000$";
+        }
+        else if(x==114){
+            document.getElementById("aa").innerHTML = "770 hp";
+            document.getElementById("bb").innerHTML = "v12,6500cc";
+            document.getElementById("cc").innerHTML = "2.9sec";
+            document.getElementById("dd").innerHTML = "507,000$";
+        }
+        else if(x==115){
+            document.getElementById("aa").innerHTML = "5000 hp";
+            document.getElementById("bb").innerHTML = "v10,5000cc";
+            document.getElementById("cc").innerHTML = "4.1sec";
+            document.getElementById("dd").innerHTML = "177,890$";
+        }
+        }
+    
